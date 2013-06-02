807b07130332a470c002acd30ba5a42639679c58
Percy <gahlotpercy@gmail.com>
2013-06-03 02:14:16 +0530

    xperia U support added

---
+++ products/pac_kumquat.mk
@@ -0,0 +1,18 @@
+# Check for target product
+ifeq (pac_kumquat,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# Common device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/pac/overlay/common/hdpi
+
+# include ParanoidAndroid common configuration
+include vendor/pac/config/pac_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/sony/kumquat/cm.mk)
+
+PRODUCT_NAME := pac_kumquat
+
+endif
