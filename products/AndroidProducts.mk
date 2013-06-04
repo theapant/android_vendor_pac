9fcdcc834629184200a8ef8ab549f72023a3f665
spacecaker <robin-barry@hotmail.com>
2013-06-04 12:26:07 +0300

    add Kumquat

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,3 +1,3 @@
-ifneq ($(filter pac_%,$(TARGET_PRODUCT)),)
-PRODUCT_MAKEFILES += $(LOCAL_DIR)/$(TARGET_PRODUCT).mk
+ifeq (pac_kumquat,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/pac_kumquat.mk
 endif
